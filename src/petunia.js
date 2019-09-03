c4fcdf120cba170d4181cdc72292856058c1fd3a
Ignacio Fuenzalida <ignacio.fuenzalida@zetatango.com>
2019-09-03 10:40:07 -0400

    add webpack config

--- src/petunia.js
+++ src/petunia.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import Crypto from './crypto.js';
 
 class Petunia {
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  // mode: 'development',
+  entry: './src/petunia.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: 'Petunia',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/env']
+        }
+      }
+    ]
+  }
+};
